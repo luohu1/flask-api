723615669c390b7fd9484f7b9861de41780029b9
luohu1 <mylhmail@163.com>
2019-07-23 22:03:28 +0800

    feat(user): Initial commit

--- apps/controller/api_v1/__init__.py
+++ apps/controller/api_v1/__init__.py
@@ -1,7 +1,9 @@
 from flask_restful import Api
 
 from apps.service.demo import DemoResource
+from apps.service.user import UserResource
 
 api_v1 = Api(prefix='/apis/v1')
 
 api_v1.add_resource(DemoResource, '/demo')
+api_v1.add_resource(UserResource, '/users')
